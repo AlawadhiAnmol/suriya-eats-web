f7551350e8c4746c2e21301e57e0a3a71d12630c
Anmol Alawadhi <alawadhi.anmol@gmail.com>
2025-06-05 15:19:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/suriya-eats-web/',
   server: {
     host: "::",
     port: 8080,
